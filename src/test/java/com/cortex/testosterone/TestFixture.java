9b84e3036ada1477727965941bd6f676d028a350
Scot Baldry <scot.baldry@gmail.com>
2012-12-17 22:14:01 +0000

    Add test for Fixture class.

---
+++ src/test/java/com/cortex/testosterone/TestFixture.java
@@ -0,0 +1,25 @@
+package com.cortex.testosterone;
+
+import junit.framework.TestCase;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class TestFixture extends TestCase {
+    public void testManualSetupNoArgsCtor() {
+        try {
+            Fixture f = new Fixture(Sample1.class.getMethod("myMethod", int.class, int.class, String.class));
+            f.addValueFactory(1, new PositiveIntegerFactory());
+            f.addValueFactory(2, new PositiveIntegerFactory());
+            f.addValueFactory(3, new RandomStringFactory());
+            f.setReturnInvariant(">0");
+            f.execute();
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+
+    public void testManualSetupArgsCtor() {
+
+    }
+}
